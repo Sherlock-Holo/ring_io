63f4c7bcb152d583942b6e01bf615160960f558a
Sherlock Holo <sherlockya@gmail.com>
2023-06-09 15:09:24 +0800

    feat: add async fn to_socket_addrs

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,2 +1,16 @@
+use std::io;
+use std::net::ToSocketAddrs;
+
+use crate::spawn_blocking;
+
 pub mod tcp;
 pub mod udp;
+
+/// just [`ToSocketAddrs`] async wrapper
+pub async fn to_socket_addrs<A: ToSocketAddrs + Send + 'static>(addr: A) -> io::Result<A::Iter>
+where
+    A: ToSocketAddrs + Send + 'static,
+    A::Iter: Send + 'static,
+{
+    spawn_blocking(move || addr.to_socket_addrs()).await
+}
